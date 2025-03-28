b60348f228df03ecad79b7d444b00fc3add74b60
tailorsdev <tailorsdevcom@gmail.com>
2025-03-27 22:40:35 -0500

    Update index.php

--- views/asignacion/index.php
+++ views/asignacion/index.php
@@ -188,7 +188,7 @@
                         Cancelar
                     </button>
                     <button
-                        @click="removerProducto"
+                        @click="removerProducto(currentProduct.id)"
                         class="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700">
                         Eliminar
                     </button>
